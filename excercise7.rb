16f7f85bf8639fcc73a40917bd2c2e1b275e4d47
Yung Dai <yungchidai@gmail.com>
2014-10-22 15:26:04 -0400

    Exercise 7 add with question 1 completed.

---
+++ excercise7.rb
@@ -0,0 +1,18 @@
+# Exercise 7 students hash initialisation
+
+students = {
+    :cohort1 => 34,
+    :cohort2 => 42,
+    :cohort3 => 22
+}
+
+# 1 For each cohort, display the cohort name and the amount of students like so. It's probably best
+# that you create a method.
+
+def student_list(students)
+  students.each do |cohort, student_number|
+    puts "#{cohort}: #{student_number} students"
+  end
+end
+
+student_list(students)
